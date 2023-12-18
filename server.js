ca9e9ad40079800ce7a937d54aa6b8b16c9fa4b1
Naoki <eigoninaritai.naokichi@gmail.com>
2023-12-18 12:50:21 +0900

    NetlifyのServerless Functionsに対応した。

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ import express from "express";
 import morgan from "morgan";
 import sourceMapSupport from "source-map-support";
 
+import serverless from "serverless-http";
+
 sourceMapSupport.install({
   retrieveSourceMap: function (source) {
     // get source file without the `file://` prefix or `?t=...` suffix
@@ -67,6 +69,9 @@ app.listen(port, async () => {
     broadcastDevReady(initialBuild);
   }
 });
+
+// Export serverless app for Netlify Functions.
+export const handler = serverless(app);
 
 /**
  * @returns {Promise<ServerBuild>}
