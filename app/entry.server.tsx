81d413b20dc8582335cb6454ad1bc3520b2a2d91
Naoki <eigoninaritai.naokichi@gmail.com>
2023-12-18 16:59:45 +0900

    デバッグコマンドを入れた。

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -4,8 +4,10 @@ import handleRequestProduction from './entry.server.netlify';
 let handleRequest;
 
 if (process.env.NODE_ENV === 'development') {
+    console.log('Running in development mode');
     handleRequest = handleRequestDevelopment;
 } else {
+    console.log('Running in production mode');
     handleRequest = handleRequestProduction;
 }
 
