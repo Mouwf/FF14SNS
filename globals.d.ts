9b11c5a2f54812e55b37f75ef1d76e647500a087
Naoki <eigoninaritai.naokichi@gmail.com>
2024-01-02 03:17:32 +0900

    インフラ系のテストを環境変数でスキップできるようにした。

--- globals.d.ts
+++ globals.d.ts
@@ -3,6 +3,7 @@ declare global {
   interface ProcessEnv {
     NODE_ENV: "development" | "production" | "test";
     PORT: string;
+    RUN_INFRA_TESTS: string;
     FIREBASE_API_KEY: string | undefined;
   }
   interface Process {
