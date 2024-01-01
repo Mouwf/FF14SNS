6724122c18bef2ddf7feb5feec891c4ff2ce0290
Naoki <eigoninaritai.naokichi@gmail.com>
2024-01-02 01:55:36 +0900

    FF14SNSのユーザーを取得するローダーの単体テストを作成した。

---
+++ tests/loaders/user/ff14-sns-user-loader.spec.ts
@@ -0,0 +1,42 @@
+import { describe, test, expect, beforeEach } from "@jest/globals";
+import FF14SnsUserLoader from "../../../app/loaders/user/ff14-sns-user-loader";
+import IAuthenticatedUserProvider from "../../../app/libraries/user/i-authenticated-user-provider";
+import MockAuthenticatedUserProvider from "../../libralies/user/mock-authenticated-user-provider";
+
+/**
+ * FF14SNSのユーザーを取得するローダー。
+ */
+let ff14SnsUserLoader: FF14SnsUserLoader;
+
+beforeEach(() => {
+    const mockAuthenticatedUserProvider = new MockAuthenticatedUserProvider();
+    ff14SnsUserLoader = new FF14SnsUserLoader(mockAuthenticatedUserProvider);
+});
+
+describe('getUser', () => {
+    test('getUser should return a FF14SnsUser object for a valid token', async () => {
+        // FF14SNSのユーザーを取得する。
+        const resultUser = await ff14SnsUserLoader.getUser('idToken');
+
+        // 結果を検証する。
+        const expectedUser = {
+            name: "UserName",
+        }
+        expect(resultUser).toEqual(expectedUser);
+    });
+
+    test('getUser should throw an error for an invalid token', async () => {
+        expect.assertions(1);
+        try {
+            await ff14SnsUserLoader.getUser('invalidIdToken');
+        } catch (error) {
+            // エラーがErrorでない場合、エラーを投げる。
+            if (!(error instanceof Error)) {
+                throw error;
+            }
+
+            // エラーを検証する。
+            expect(error.message).toBe('Invalid token.');
+        }
+    });
+});
