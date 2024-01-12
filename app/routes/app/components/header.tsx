b292bbbbaf81c04a8dbbbe91ee308f9ce903e71e
Naoki <eigoninaritai.naokichi@gmail.com>
2024-01-12 15:43:39 +0900

    関数コンポーネントにスタイルを設定するサンプルを追加した。

--- app/routes/app/components/header.tsx
+++ app/routes/app/components/header.tsx
@@ -1,12 +1,13 @@
 import { Form } from "@remix-run/react";
 import useSnsUser from "../../../contexts/user/use-sns-user";
+import styles from "./header.module.css";
 
 export default function Header() {
     const ff14SnsUser = useSnsUser();
 
     return (
-        <header>
-            <h1>FF14 Header</h1>
+        <header className={styles["header"]}>
+            <h1 className={styles["test-class"]}>FF14 Header</h1>
             <p>{ff14SnsUser.name}</p>
             <Form method="post">
                 <button type="submit">ログアウト</button>
