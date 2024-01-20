81cca924e46b53b0f36b4ab82799caca406e8b6e
Naoki <eigoninaritai.naokichi@gmail.com>
2024-01-20 22:02:31 +0900

    リリース情報取得に関する単体テストを追加した。

---
+++ tests/libraries/post/release-information-getter.spec.ts
@@ -0,0 +1,51 @@
+import { describe, test, expect, beforeEach } from "@jest/globals";
+import MockReleaseInformationRepository from "../../repositories/post/mock-release-information-repository";
+import ReleaseInformationGetter from "../../../app/libraries/post/release-information-getter";
+
+/**
+ * リリース情報を取得するクラス。
+ */
+let releaseInformationGetter: ReleaseInformationGetter;
+
+beforeEach(() => {
+    const mockReleaseInformationRepository = new MockReleaseInformationRepository();
+    releaseInformationGetter = new ReleaseInformationGetter(mockReleaseInformationRepository);
+});
+
+describe("getReleaseInformation", () => {
+    test("getReleaseInformation should return a release information.", async () => {
+        // リリース情報を取得する。
+        const releaseId = 1;
+        const response = await releaseInformationGetter.getReleaseInformation(releaseId);
+
+        // 結果を検証する。
+        const expectedReleaseInformation = {
+            id: 1,
+            releaseVersion: "2.5",
+            releaseName: "リリース2.5",
+            createdAt: new Date(),
+        }
+        expect(response.id).toBe(expectedReleaseInformation.id);
+        expect(response.releaseVersion).toBe(expectedReleaseInformation.releaseVersion);
+        expect(response.releaseName).toBe(expectedReleaseInformation.releaseName);
+        expect(response.createdAt).toBeInstanceOf(Date);
+    });
+});
+
+describe("getAllReleaseInformation", () => {
+    test("getAllReleaseInformation should return release informations.", async () => {
+        // リリース情報を取得する。
+        const response = await releaseInformationGetter.getAllReleaseInformation();
+
+        // 結果を検証する。
+        expect(response.length).toBe(5);
+        expect(response[0].id).toBe(1);
+        expect(response[0].releaseVersion).toBe("2.5");
+        expect(response[0].releaseName).toBe("リリース2.5");
+        expect(response[0].createdAt).toBeInstanceOf(Date);
+        expect(response[1].id).toBe(2);
+        expect(response[1].releaseVersion).toBe("2.4");
+        expect(response[1].releaseName).toBe("リリース2.4");
+        expect(response[1].createdAt).toBeInstanceOf(Date);
+    });
+});
--- tests/routes/app.post-message/route.spec.ts
+++ tests/routes/app.post-message/route.spec.ts
@@ -1,7 +1,7 @@
 import { describe, test, expect, beforeEach } from "@jest/globals";
 import { AppLoadContext } from "@netlify/remix-runtime";
 import appLoadContext from "../../dependency-injector/app-load-context";
-import { action } from "../../../app/routes/app.post-message/route";
+import { loader, action } from "../../../app/routes/app.post-message/route";
 import { newlyPostedPostCookie } from "../../../app/cookies.server";
 
 /**
@@ -10,27 +10,54 @@ import { newlyPostedPostCookie } from "../../../app/cookies.server";
 let request: Request;
 
 /**
+ * ボディ付きのモックリクエスト。
+ */
+let requestWithBody: Request;
+
+/**
  * モックコンテキスト。
  */
 let context: AppLoadContext;
 
 beforeEach(async () => {
-    request = new Request("https://example.com", {
+    request = new Request("https://example.com");
+    requestWithBody = new Request("https://example.com", {
         method: "POST",
         body: new URLSearchParams({
-            releaseVersion: "パッチ5",
-            tag: "タグ2",
+            releaseInformationId: "パッチ5",
             content: "アクション経由の投稿テスト！",
         }),
     });
     context = appLoadContext;
 });
 
-describe('action', () => {
-    test('action should set cookie about post.', async () => {
+describe("loader", () => {
+    test("loader should return release information.", async () => {
+        // ローダーを実行し、結果を取得する。
+        const response = await loader({
+            request: request,
+            params: {},
+            context,
+        });
+
+        // 検証に必要な情報を取得する。
+        const resultAllReleaseInformation = await response.json();
+
+        // 結果を検証する。
+        resultAllReleaseInformation.map((releaseInformation) => {
+            expect(releaseInformation.id).toBeDefined();
+            expect(releaseInformation.releaseVersion).toBeDefined();
+            expect(releaseInformation.releaseName).toBeDefined();
+            expect(new Date(releaseInformation.createdAt)).toBeInstanceOf(Date);
+        });
+    });
+});
+
+describe("action", () => {
+    test("action should set cookie about post.", async () => {
         // アクションを実行し、結果を取得する。
         const response = await action({
-            request,
+            request: requestWithBody,
             params: {},
             context,
         });
@@ -45,7 +72,6 @@ describe('action', () => {
         expect(location).toBe("/app");
         expect(cookie).toEqual({
             releaseVersion: "パッチ5",
-            tag: "タグ2",
             content: "アクション経由の投稿テスト！",
             isPosted: true,
         });
