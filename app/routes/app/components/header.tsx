9d15f14d6407be1e0036c41ae74dfe26cac5a133
hiro <freedom_of_hr@yahoo.co.jp>
2024-01-21 18:07:18 +0900

    ざっくりとヘッダーのスタイリング。

--- app/routes/app/components/header.tsx
+++ app/routes/app/components/header.tsx
@@ -7,8 +7,8 @@ export default function Header() {
 
     return (
         <header className={styles["header"]}>
-            <div className={styles["test11"]}>
-                <h1 className={styles["test-class"]}>FF14 Header</h1>
+            <div className={styles["header-contents-area"]}>
+                <h1 className={styles["test-class"]}>FF14 SNS</h1>
                 <p>{ff14SnsUser.name}</p>
                 <Form method="post">
                     <button type="submit">ログアウト</button>
