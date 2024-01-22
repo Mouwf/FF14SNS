08ab1318d87db62f166e3fc21a13e8265eb97d30
Naoki <eigoninaritai.naokichi@gmail.com>
2024-01-23 02:04:23 +0900

    Expressで動作する最小構成にした。

--- remix.config.js
+++ remix.config.js
@@ -1,18 +1,9 @@
-const productionConfig = {
-  ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
-  serverModuleFormat: "esm",
-}
-
-const developmentConfig = {
-  ignoredRouteFiles: ["**/.*"],
-  serverModuleFormat: "esm",
-}
-
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  ...(process.env.NODE_ENV === 'development' ? developmentConfig : productionConfig),
-}
+  ignoredRouteFiles: ["**/.*"],
+  serverModuleFormat: "esm",
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // publicPath: "/build/",
+  // serverBuildPath: "build/index.js",
+};
