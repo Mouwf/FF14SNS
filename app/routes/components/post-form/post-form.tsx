6a8f8a3c444d87a6dfcce896eed61d818b89c4b8
hiro <freedom_of_hr@yahoo.co.jp>
2024-03-11 20:29:13 +0900

    最新の投稿フォームとマージした

--- app/routes/components/post-form/post-form.tsx
+++ app/routes/components/post-form/post-form.tsx
@@ -2,6 +2,7 @@ import { Form } from "@remix-run/react";
 import useSnsUser from "../../../contexts/user/use-sns-user";
 import PostUserProfileImage from "../post-display/post-user-profile-image";
 import { ReactNode } from "react";
+import styles from "./post-form.module.css";
 
 /**
  * 投稿フォーム。
@@ -17,15 +18,17 @@ export default function PostForm({
     const snsUser = useSnsUser();
 
     return (
-        <Form method="post">
+        <Form className={styles["post-message-area"]} method="post">
             <PostUserProfileImage />
             <p>{snsUser.userName}</p>
-            {children}
-            <div>
-                <textarea name="content" placeholder="メッセージを入力してください" />
-            </div>
-            <div>
-                <button type="submit">{submitMessage}</button>
+            <div className={styles["post-message-container"]}>
+                {children}
+                <div className={styles["post-message"]}>
+                    <textarea name="content" placeholder="今日はどんな冒険をしましたか？" />
+                </div>
+                <div className={styles["post-message-btn"]}>
+                    <button type="submit">{submitMessage}</button>
+                </div>
             </div>
         </Form>
     );
